c8ba630a8c38d0b89facf3d45f70d61ece8c78c1
mehdiboussaid97-ai <mehdiboussaid97@gmail.com>
2026-04-01 14:59:10 +0200

    Add files via upload

--- Tp3/tp3.py
+++ Tp3/tp3.py
@@ -2,6 +2,24 @@ import numpy as np
 from matplotlib import pyplot as plt 
 
 #%%
+
 def onde(A,c,lbda,x,t):
-    ffggsdf
+    '''
+    A : Amplitude
+    c : vitesse
+    lbda : lambda
+    x : position
+    t : temp
+    '''
     
+    return A * (np.cos((( 2 * np.pi ) / lbda) * (x-c*t)))
+
+xx = np.linspace(-10, 10)
+yy = []
+for x in xx:
+    yy = np.append(yy,onde(5, 300, 20, x, 0) )
+    
+print(xx,yy)    
+plt.plot(xx, yy)
+plt.show()
+
