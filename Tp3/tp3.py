7e1bd5ce083946d8a663cd14891676548497c215
mehdiboussaid97-ai <mehdiboussaid97@gmail.com>
2026-04-01 15:24:54 +0200

    Add files via upload

--- Tp3/tp3.py
+++ Tp3/tp3.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt 
 
 #%%
+#Q1
 
 def onde(A,c,lbda,x,t):
     '''
@@ -14,12 +15,48 @@ def onde(A,c,lbda,x,t):
     
     return A * (np.cos((( 2 * np.pi ) / lbda) * (x-c*t)))
 
-xx = np.linspace(-10, 10)
+#%%
+#Q2
+xx = np.linspace(-1, 1)
 yy = []
 for x in xx:
-    yy = np.append(yy,onde(5, 300, 20, x, 0) )
+    yy = np.append(yy,onde(5, 1, 1, x, 0) )
     
 print(xx,yy)    
 plt.plot(xx, yy)
 plt.show()
 
+#%%
+#Q3
+
+tt = np.linspace(-1, 1,1000)
+x1 = 0
+x2 = np.pi/2
+x3 = np.pi
+x4 = 3 * np.pi/2
+yy1 = []
+yy2 = []
+yy3 = []
+yy4 = []
+for t in tt:
+    yy1 = np.append(yy1,onde(5, 1, 1, x1, t) )
+    yy2 = np.append(yy2,onde(5, 1, 1, x2, t) )
+    yy3 = np.append(yy3,onde(5, 1, 1, x3, t) )
+    yy4 = np.append(yy4,onde(5, 1, 1, x4, t) )
+
+figur = plt.figure()    
+plt.subplot(2, 2,1)
+plt.plot(tt, yy1)
+
+
+plt.subplot(2, 2,2)
+plt.plot(tt, yy2)
+
+plt.subplot(2, 2,3)
+plt.plot(tt, yy3)
+
+
+plt.subplot(2, 2,4)
+plt.plot(tt, yy4)
+
+    
